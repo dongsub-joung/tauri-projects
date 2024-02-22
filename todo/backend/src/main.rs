0ed565a8c2dfc2f0d260139b55e74659fabf9779
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-02-23 05:25:42 +0900

    base

--- todo/backend/src/main.rs
+++ todo/backend/src/main.rs
@@ -1,18 +1,29 @@
 use axum::{
     routing::get, Router,
 };
+use sqlx::SqlitePool;
 use std::net::SocketAddr;
+use axum_error::Result;
+// use sqlx::sqlite::SqlitePool;
+use tower_http::cors::CorsLayer
 
 #[tokio::main]
-async fn main(){
-    let app= Router::new().route("/",get(index));
+async fn main() -> Result<()>{
+    let _= dotenv::dotenv();
+    let url= std::env::var("DATABASE_URL")?;
+    let pool= SqlitePool::connect(&url).await?;
+
+    let app= Router::new()
+        .route("/",get(list))
+        .with_state(pool)
+        .layer(CorsLayer::very_permissive());
+    
     let address= SocketAddr::from(([0,0,0,0], 8000));
-    axum::Server::bind(&address)
+    Ok(axum::Server::bind(&address)
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .await?)
 }
 
-async fn index() -> String{
+async fn list() -> String{
     format!("Hello")
 }
